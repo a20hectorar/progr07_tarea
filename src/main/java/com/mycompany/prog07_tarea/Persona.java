5f344ab0eac4500bb714513847405ae3b3d9d204
Hector <a20hectorar@iessanclemente.net>
2023-02-25 22:28:12 +0100

    token copiado en principal

--- src/main/java/com/mycompany/prog07_tarea/Persona.java
+++ src/main/java/com/mycompany/prog07_tarea/Persona.java
@@ -11,13 +11,39 @@ package com.mycompany.prog07_tarea;
  * @author Hector
  */
 public class Persona {
-    String nombre;
-    String apellidos;
-    String dni;
+    private String nombre;
+    private String apellidos;
+    private String dni;
     
     public Persona(String nombre, String apellidos,String dni){
         this.nombre=nombre;
         this.apellidos=apellidos;
         this.dni=dni;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    
 }
